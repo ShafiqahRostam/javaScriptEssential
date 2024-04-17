19f8ccdf8578358468175c2a1a6a2ca072608742
Shafiqah <shafiqah.rostam@hotmail.sg>
2024-04-17 00:43:56 -0400

    BookSystem practice task

--- BookSystem/book_system.js
+++ BookSystem/book_system.js
@@ -20,12 +20,19 @@ function addBook() {
     }
 }
 
+function deleteBook(index) {
+    //alert('book to be deleted is ' + (index + 1));
+    books.splice(index, 1);
+    showBooks();
+}
+
 function showBooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+    const booksDiv = books.map((book, index) => `<h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name:</strong> ${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>`).join('');
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>
+        <p><button onclick="deleteBook(${index})">Delete</button></p>`).join('');
     document.getElementById('books').innerHTML = booksDiv;
 }
 
